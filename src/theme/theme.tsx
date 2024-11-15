528d0cd14f3bdb2d3550ae53de7c8edd6df0bbfb
박지연 <163213733+j02on@users.noreply.github.com>
2024-11-15 09:54:22 +0900

    FEAT :: Design system

---
+++ src/theme/theme.tsx
@@ -0,0 +1,19 @@
+export const theme = {
+  color: {
+    white: '#ffffff',
+    main: {
+      1: '#FFEAEE',
+      2: '#F8CDD5',
+      3: '#FFAABA',
+      4: '#FF99AC',
+      5: '#FF8097',
+    },
+    gray: {
+      1: '#000000',
+      2: '#647A73',
+      3: '#8CA29A',
+      4: '#AAC3BA',
+      5: 'rgb(174, 172, 172, 0.46)',
+    },
+  },
+};

7c09bb906061365c0c60e9c07e2909642d5fc98a
박지연 <163213733+j02on@users.noreply.github.com>
2024-11-15 10:39:20 +0900

    FEAT :: SignUp 퍼블리싱
    폰트 미적용 상태입니다.

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,106 @@
+import styled from 'styled-components';
+import { theme } from '../theme';
+import { Inputs } from '../components/user/Inputs';
+import { Button } from '../components/Button';
+import { useState } from 'react';
+
+export const SignUp = () => {
+  const [isType, setIsType] = useState<boolean>(false);
+
+  return (
+    <SignUpContainer>
+      <Title>회원가입</Title>
+      <InputsContainer>
+        <BtnInputContainer>
+          <Inputs
+            setIsType={false}
+            isType={isType}
+            placeholder="이메일을 입력하세요"
+            label="이메일"
+          />
+          <Btn>send</Btn>
+        </BtnInputContainer>
+        <BtnInputContainer>
+          <Inputs
+            setIsType={false}
+            isType={isType}
+            placeholder="인증코드를 입력하세요"
+            label="인증코드"
+          />
+          <Btn>Check</Btn>
+        </BtnInputContainer>
+        <Inputs
+          setIsType={true}
+          isType={isType}
+          placeholder="닉네임을 입력하세요"
+          label="닉네임"
+        />
+
+        <Inputs
+          setIsType={true}
+          isType={isType}
+          placeholder="비밀번호를 입력하세요"
+          label="비밀번호"
+        />
+        <Inputs
+          setIsType={true}
+          isType={isType}
+          placeholder="비밀번호를 다시 입력하세요"
+          label="비밀번호 확인"
+        />
+      </InputsContainer>
+      <Button children="회원가입" />
+    </SignUpContainer>
+  );
+};
+
+const BtnInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: end;
+  width: 364px;
+`;
+
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: 700;
+  color: ${theme.color.main[3]};
+`;
+
+const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+`;
+
+const Btn = styled.button`
+  &:active {
+    outline: none;
+    border: none;
+  }
+  &:hover {
+    outline: none;
+    border: none;
+  }
+  color: ${theme.color.white};
+  font-weight: 700;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 30px;
+  border-radius: 100px;
+  background-color: ${theme.color.main[3]};
+  cursor: pointer;
+`;
+
+const SignUpContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  gap: 70px;
+  margin-top: 60px;
+`;
